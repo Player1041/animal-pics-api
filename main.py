cc1621e35d980a1d1a537fd05c03d1fd84c6a544
Player1041 <redacteddoesit@gmail.com>
2022-03-22 21:49:59 +0000

    owo

---
+++ main.py
@@ -0,0 +1,11 @@
+from fastapi import FastAPI
+from fastapi.responses import FileResponse
+import os
+import random
+
+random_otter = random.choice(os.listdir("otters"))
+app = FastAPI()
+
+@app.get(/)
+async def random():
+    return random_otter
