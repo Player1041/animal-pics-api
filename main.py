dc82db9831fe11eefe609d565984fce7ed5cd9ec
Player <redacteddoesit@gmail.com>
2022-04-15 18:18:34 +0100

    d

--- main.py
+++ main.py
@@ -2,11 +2,18 @@ from random import choice
 from glob import glob
 
 from fastapi import FastAPI
-from fastapi.responses import FileResponse
+from fastapi.responses import FileResponse, HTMLResponse
+from fastapi.staticfiles import StaticFiles
 
 app = FastAPI()
 
+app.mount("/static", StaticFiles(directory="static"), name="static")
 
 @app.get("/")
-async def main():
+async def site(response_class=HTMLResponse):
+    return FileResponse("index.html")
+
+
+@app.get("/otters")
+async def otter():
     return FileResponse(choice(glob('otters/*')))
